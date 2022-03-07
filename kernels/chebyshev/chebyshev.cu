fe063ad912e7e6240fb189c6c5848407b0f31e7c
Sam Hirsch <sam.hirsch66@gmail.com>
2022-03-07 15:59:41 -0700

    fixed chebyshev code

--- kernels/chebyshev/chebyshev.cu
+++ kernels/chebyshev/chebyshev.cu
@@ -3,53 +3,69 @@
 #include "brick.h"
 
 // $START naive
-__global__ void chebyshev_naive(bElem (*Ac)[STRIDE1][STRIDE0], bElem (*Ap)[STRIDE1][STRIDE0], bElem (*Dinv)[STRIDE1][STRIDE0], bElem (*RHS)[STRIDE1][STRIDE0], bElem (*out)[STRIDE1][STRIDE0], bElem *c, bElem (*coeff)[4][4]) {
-  const size_t radius = $SIZE;
+__global__ void chebyshev_naive(bElem (*Ac)[STRIDE1][STRIDE0], bElem (*Ap)[STRIDE1][STRIDE0], bElem (*Dinv)[STRIDE1][STRIDE0], bElem (*RHS)[STRIDE1][STRIDE0], bElem (*out)[STRIDE1][STRIDE0], bElem *c) {
   unsigned i = OFF0 + (blockIdx_x) * TILE0 + threadIdx_x;
   unsigned j = OFF1 + (blockIdx_y) * TILE1 + threadIdx_y;
   unsigned k = OFF2 + (blockIdx_z) * TILE2 + threadIdx_z;
 
-  bElem base = 0;
-  #pragma unroll
-  for (int i_diff = -radius; i_diff <= radius; i_diff++) {
-    #pragma unroll
-    for (int j_diff = -radius; j_diff <= radius; j_diff++) {
-      #pragma unroll
-      for (int k_diff = -radius; k_diff <= radius; k_diff++) {
-	base += coeff[i_diff + radius][j_diff + radius][k_diff + radius] * (Ac[i + i_diff][j + j_diff][k + k_diff]);
-      }
-    }
-  }
-  out[i][j][k] = Ac[i][j][k] + c[0] * (Ac[i][j][k] + Ap[i][j][k]) + c[1] * Dinv[i][j][k] *
-    (RHS[i][j][k] + (Ac[i][j][k] + c[2] * base));
+  out[k][j][i] = 
+    Ac[k][j][i] + c[0] * (Ac[k][j][i] + Ap[k][j][i]) +
+    c[1] * Dinv[k][j][i] *
+    (RHS[k][j][i] +
+    (Ac[k][j][i] + 
+    c[2] * 
+      (
+        0.03 * (Ac[k - 1][j - 1][i - 1] + Ac[k - 1][j - 1][i + 1] +
+                Ac[k - 1][j + 1][i - 1] + Ac[k - 1][j + 1][i + 1] +
+                Ac[k + 1][j - 1][i - 1] + Ac[k + 1][j - 1][i + 1] +
+                Ac[k + 1][j + 1][i - 1] + Ac[k + 1][j + 1][i + 1]) +
+        0.1 * (Ac[k - 1][j - 1][i] + Ac[k - 1][j][i - 1] +
+               Ac[k - 1][j][i + 1] + Ac[k - 1][j + 1][i] +
+               Ac[k][j - 1][i - 1] + Ac[k][j - 1][i + 1] +
+               Ac[k][j + 1][i - 1] + Ac[k][j + 1][i + 1] +
+               Ac[k + 1][j - 1][i] + Ac[k + 1][j][i - 1] +
+               Ac[k + 1][j][i + 1] + Ac[k + 1][j + 1][i]) +
+        0.46 * (Ac[k - 1][j][i] + Ac[k][j - 1][i] + Ac[k][j][i - 1] +
+                Ac[k + 1][j][i] + Ac[k][j + 1][i] + Ac[k][j][i + 1]) +
+        4.26 * Ac[k][j][i]
+               )));
 }
 // $END naive
 
 // $START naive-bricks
-__global__ void chebyshev_naive_bricks(unsigned (*grid)[NAIVE_BSTRIDE1][NAIVE_BSTRIDE0], BType Ac, BType Ap, BType Dinv, BType RHS, BType out, bElem *c, bElem (*coeff)[4][4]) {
-  const size_t radius = $SIZE;
+__global__ void chebyshev_naive_bricks(unsigned (*grid)[NAIVE_BSTRIDE1][NAIVE_BSTRIDE0], BType Ac, BType Ap, BType Dinv, BType RHS, BType out, bElem *c) {
   unsigned b = grid[blockIdx_z + GB2][blockIdx_y + GB1][blockIdx_x + GB0];
   unsigned i = threadIdx_x;
   unsigned j = threadIdx_y;
   unsigned k = threadIdx_z;
 
-  bElem base = 0;
-  #pragma unroll
-  for (int i_diff = -radius; i_diff <= radius; i_diff++) {
-    #pragma unroll
-    for (int j_diff = -radius; j_diff <= radius; j_diff++) {
-      #pragma unroll
-      for (int k_diff = -radius; k_diff <= radius; k_diff++) {
-	base += coeff[i_diff + radius][j_diff + radius][k_diff + radius] * (Ac[b][i + i_diff][j + j_diff][k + k_diff]);
-      }
-    }
-  }
-  out[b][i][j][k] = Ac[b][i][j][k] + c[0] * (Ac[b][i][j][k] + Ap[b][i][j][k]) + c[1] * Dinv[b][i][j][k] * (RHS[b][i][j][k] + (Ac[b][i][j][k] + c[2] * base));
+  out[b][k][j][i] = 
+    Ac[b][k][j][i] + c[0] * (Ac[b][k][j][i] + Ap[b][k][j][i]) +
+    c[1] * Dinv[b][k][j][i] *
+    (RHS[b][k][j][i] +
+    (Ac[b][k][j][i] + 
+    c[2] * 
+      (
+        0.03 * (Ac[b][k - 1][j - 1][i - 1] + Ac[b][k - 1][j - 1][i + 1] +
+                Ac[b][k - 1][j + 1][i - 1] + Ac[b][k - 1][j + 1][i + 1] +
+                Ac[b][k + 1][j - 1][i - 1] + Ac[b][k + 1][j - 1][i + 1] +
+                Ac[b][k + 1][j + 1][i - 1] + Ac[b][k + 1][j + 1][i + 1]) +
+        0.1 * (Ac[b][k - 1][j - 1][i] + Ac[b][k - 1][j][i - 1] +
+               Ac[b][k - 1][j][i + 1] + Ac[b][k - 1][j + 1][i] +
+               Ac[b][k][j - 1][i - 1] + Ac[b][k][j - 1][i + 1] +
+               Ac[b][k][j + 1][i - 1] + Ac[b][k][j + 1][i + 1] +
+               Ac[b][k + 1][j - 1][i] + Ac[b][k + 1][j][i - 1] +
+               Ac[b][k + 1][j][i + 1] + Ac[b][k + 1][j + 1][i]) +
+        0.46 * (Ac[b][k - 1][j][i] + Ac[b][k][j - 1][i] + Ac[b][k][j][i - 1] +
+                Ac[b][k + 1][j][i] + Ac[b][k][j + 1][i] + Ac[b][k][j][i + 1]) +
+        4.26 * Ac[b][k][j][i]
+               )));
+}
 }
 // $END naive-bricks
 
 // $START codegen-bricks
-__global__ void chebyshev_codegen_bricks(unsigned (*grid)[NAIVE_BSTRIDE1][NAIVE_BSTRIDE0], BType Ac, BType Ap, BType Dinv, BType RHS, BType out, bElem *c, bElem (*coeff)[4][4]) {
+__global__ void chebyshev_codegen_bricks(unsigned (*grid)[NAIVE_BSTRIDE1][NAIVE_BSTRIDE0], BType Ac, BType Ap, BType Dinv, BType RHS, BType out, bElem *c) {
   unsigned b = grid[blockIdx_z + GB2][blockIdx_y + GB1][blockIdx_x + GB0];
   brick("$PYTHON", VSVEC, (TILE2, TILE1, TILE0), (FOLD), b);
 }
@@ -62,7 +78,7 @@ __global__ void chebyshev_codegen_bricks(unsigned (*grid)[NAIVE_BSTRIDE1][NAIVE_
 #define RHS(a, b, c) RHS_arr[c][b][a]
 #define out(a, b, c) out_arr[c][b][a]
 
-__global__ void chebyshev_codegen(bElem (*Ac_arr)[STRIDE1][STRIDE0], bElem (*Ap_arr)[STRIDE1][STRIDE0], bElem (*Dinv_arr)[STRIDE1][STRIDE0], bElem (*RHS_arr)[STRIDE1][STRIDE0], bElem (*out_arr)[STRIDE1][STRIDE0], bElem *c, bElem (*coeff)[4][4]) {
+__global__ void chebyshev_codegen(bElem (*Ac_arr)[STRIDE1][STRIDE0], bElem (*Ap_arr)[STRIDE1][STRIDE0], bElem (*Dinv_arr)[STRIDE1][STRIDE0], bElem (*RHS_arr)[STRIDE1][STRIDE0], bElem (*out_arr)[STRIDE1][STRIDE0], bElem *c) {
   long k = OFF2 + (blockIdx_z * TILE2);
   long j = OFF1 + (blockIdx_y * TILE1);
   long i = OFF0 + (blockIdx_x * TILE0);
